28129b435081f6726faec1fd482aa9e94195d89e
DonChatelain <djchatelain@gmail.com>
2016-02-23 16:19:22 -0800

    cleans up js code and lil fix in css

--- js/index.js
+++ js/index.js
@@ -1,28 +1,33 @@
-var navOpen = false;
 $(function() {
-	
-	$('.ham-menu').on('click', function() {
-		$('.menu-box').slideToggle();
-	});
 
-	$('.menu-box ul a').hover(function() {
-		$(this).parents('li').addClass('button-highlight');
-		$(this).addClass('nav-highlight');
-	}, function() {
-		$(this).parents('li').removeClass('button-highlight');
-		$(this).removeClass('nav-highlight');
-	});
+	var buttonController = {};
 
-	$('.contact-button').hover(function() {
-		$(this).addClass('button-highlight');
-	}, function() {
-		$(this).removeClass('button-highlight');
-	});
+	buttonController.setListeners = function() {
+		$('.ham-menu').on('click', function() {
+			$('.menu-box').slideToggle();
+		});
 
-	$('.more-info').on('click', function() {
-		$('.works p:nth-child(7)').toggle();
-	});
+		$('.menu-box ul a').hover(function() {
+			$(this).parents('li').addClass('button-highlight');
+			$(this).addClass('nav-highlight');
+		}, function() {
+			$(this).parents('li').removeClass('button-highlight');
+			$(this).removeClass('nav-highlight');
+		});
 
+		$('.contact-button').hover(function() {
+			$(this).addClass('button-highlight');
+		}, function() {
+			$(this).removeClass('button-highlight');
+		});
 
+		$('.more-info').on('click', function() {
+			$('.works p:nth-child(7)').toggle();
+		});
+	};
+
+	//=============================================  EXECUTIVES ===============================
+
+	buttonController.setListeners();
 
 });
--- js/works.js
+++ js/works.js
@@ -1,5 +1,5 @@
 $(function() {
-
+	
 	var repos;
 	var outhtml;
 	var username = "DonChatelain";
